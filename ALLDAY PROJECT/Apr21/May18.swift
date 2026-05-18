dadf6173340f6e79ca31c7baee95bd0ac09eab24
min4034415 <min4034415@gmail.com>
2026-05-18 16:31:21 +0900

    ㅁㅁ

---
+++ ALLDAY PROJECT/Apr21/May18.swift
@@ -0,0 +1,15 @@
+//
+//  May18.swift
+//  ALLDAY PROJECT
+//
+//  Created by Ouimin Lee on 5/18/26.
+//
+
+import Foundation
+
+class May18 {
+    var example:[Int] = []
+    func minJumps(_ arr: [Int]) -> Int {
+        0
+    }
+}
