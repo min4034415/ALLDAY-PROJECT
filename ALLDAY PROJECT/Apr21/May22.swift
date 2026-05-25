f89771e50a6e55cd9b19ad04094d6108387b9708
min4034415 <65104668+min4034415@users.noreply.github.com>
2026-05-25 19:15:20 +0900

    commiting

--- ALLDAY PROJECT/Apr21/May22.swift
+++ ALLDAY PROJECT/Apr21/May22.swift
@@ -8,5 +8,8 @@
 import Foundation
 
 class May22 {
-    let example = [
+    let example = [([4,5,6,7,0,1,2],0),([4,5,6,7,0,1,2],3),([1],0)]
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        return(nums.firstIndex(of: target)!)
+    }
 }
